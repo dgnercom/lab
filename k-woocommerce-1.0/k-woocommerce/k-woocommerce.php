ef3560a98b8c8229e5a097065c3e167822395d0d
dgner <81064063+dgnercom@users.noreply.github.com>
2021-04-01 21:21:58 +0900

    Update k-woocommerce.php

--- k-woocommerce-1.0/k-woocommerce/k-woocommerce.php
+++ k-woocommerce-1.0/k-woocommerce/k-woocommerce.php
@@ -8,13 +8,6 @@
  * Author URI: https://dgner.com
  * License: GPL3
  */
-function dgner_settings_link( $links ) {  // 플러그인 전용 도네이션 링크이므로 function.php에 포함하지 마세요. 간단한 스니펫이라 응용 및 공유는 100% 자유로우며 상단 Plugin URI에 기입된 출저 표기만 해주시기 바랍니다.
-    $donate_link = __( '<a target="_blank" style="color:#a050c8;font-weight:bold;" href="https://dgner.com/wp-content/uploads/dgner-korea-donation-link-designer-community-for-design-news-design-education-design-events-design-influencers-design-tools-design-forum-and-social-network.png">커피 한잔</a>', 'k-woocommerce' );
-    array_unshift($links, $donate_link);
-    return $links;
-}
-$dgner_plugin = plugin_basename(__FILE__);
-add_filter("plugin_action_links_$dgner_plugin", 'dgner_settings_link' );
 add_action( 'wp_enqueue_scripts', 'dgner_wp_enqueue_scripts' ); // function.php에 직접 기입하는 경우, 18번째 줄이 Start Point 입니다. 카카오(다음) 우편번호 서비스 JS API를 특정 페이지에만 불러옵니다.
 function dgner_wp_enqueue_scripts() {        
     if ( ! is_account_page() && ! is_checkout() ) {
